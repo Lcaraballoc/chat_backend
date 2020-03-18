e0f1cff82d302b5aed28adfa39adc05900368c1c
Luis Caraballo <luisred61@gmail.com>
2020-03-17 23:57:13 -0500

    [ADD] MongoDB connection established

--- components/message/store.js
+++ components/message/store.js
@@ -1,7 +1,27 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
+const {config} = require('../../config');
+
+const USER = config.dbUser;
+const HOST = config.dbHost;
+const PASSWD = config.dbPassword;
+const DBNAME = config.dbName;
+const DBPORT = config.dbPort;
+
+db.Promise= global.Promise;
+db.connect(`mongodb+srv://${USER}:${PASSWD}@${HOST}/${DBNAME}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+/* console.log(USER + HOST + PASSWD + DBNAME + DBPORT) */
+
+console.log("Conectada con exito")
 
 function addMessage(message) {
-    list.push(message);
+    /* list.push(message); */
+    const myMessage = new Model(message)
+    myMessage.save();
 }
 
 function getMessage() {
